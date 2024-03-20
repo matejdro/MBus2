631d880fc986deac1a3106f20bef4d5ccd17ff2a
Matej Drobnič <matej@matejdro.com>
2024-03-20 11:11:52 +0100

    fix(common-retrofit): parse response error message

--- common-retrofit/src/main/kotlin/com/matejdro/mbus/network/models/BackendError.kt
+++ common-retrofit/src/main/kotlin/com/matejdro/mbus/network/models/BackendError.kt
@@ -9,4 +9,13 @@ data class BackendError(
    val message: String?,
    @Json(name = "MessageDetail")
    val messageDetail: String?,
-)
+   val response: Response?,
+) {
+   @JsonClass(generateAdapter = true)
+   data class Response(
+      @Json(name = "Message")
+      val message: String?,
+      @Json(name = "Status")
+      val status: Int?,
+   )
+}
