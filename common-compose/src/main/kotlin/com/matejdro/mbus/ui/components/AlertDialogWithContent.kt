449bfaf73dd449a86f399d058cdac77aaa799dce
Matej Drobnič <matej@matejdro.com>
2024-04-24 10:02:06 +0200

    fix(common-compose): alert dialog cut off on small height

--- common-compose/src/main/kotlin/com/matejdro/mbus/ui/components/AlertDialogWithContent.kt
+++ common-compose/src/main/kotlin/com/matejdro/mbus/ui/components/AlertDialogWithContent.kt
@@ -1,11 +1,9 @@
 package com.matejdro.mbus.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
@@ -19,10 +17,18 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.constrain
 import androidx.compose.ui.unit.dp
 import com.airbnb.android.showkase.annotation.ShowkaseComposable
 import com.matejdro.mbus.ui.debugging.PreviewTheme
+import kotlin.math.max
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,30 +49,62 @@ fun AlertDialogWithContent(
             color = AlertDialogDefaults.containerColor,
             tonalElevation = AlertDialogDefaults.TonalElevation,
          ) {
-            Column(
+            Layout(
                modifier = Modifier
-                  .padding(24.dp)
-            ) {
-               title?.let {
-                  CompositionLocalProvider(
-                     LocalTextStyle provides MaterialTheme.typography.headlineSmall
-                  ) {
-                     it()
+                  .padding(24.dp),
+               measurePolicy = DialogMeasuePolicy,
+               content = {
+                  Box(Modifier.padding(bottom = 16.dp)) {
+                     title?.let {
+                        CompositionLocalProvider(
+                           LocalTextStyle provides MaterialTheme.typography.headlineSmall
+                        ) {
+                           it()
+                        }
+                     }
                   }
 
-                  Spacer(Modifier.height(16.dp))
-               }
+                  Box {
+                     content()
+                  }
 
-               content()
-
-               Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-                  dismissButton()
-                  confirmButton()
+                  Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                     dismissButton()
+                     confirmButton()
+                  }
                }
-            }
+            )
          }
       }
    )
+}
+
+private object DialogMeasuePolicy : MeasurePolicy {
+   override fun MeasureScope.measure(measurables: List<Measurable>, constraints: Constraints): MeasureResult {
+      val title = measurables.elementAt(0)
+      val content = measurables.elementAt(1)
+      val buttons = measurables.elementAt(2)
+
+      val titlePlaceable = title.measure(constraints)
+      val buttonsPlaceable = buttons.measure(constraints)
+
+      val contentConstraints = constraints.constrain(
+         Constraints(
+            maxHeight = constraints.maxHeight - titlePlaceable.height - buttonsPlaceable.height
+         )
+      )
+
+      val contentPlaceable = content.measure(contentConstraints)
+
+      return layout(
+         width = max(titlePlaceable.width, max(contentPlaceable.width, buttonsPlaceable.width)),
+         height = titlePlaceable.height + contentPlaceable.height + buttonsPlaceable.height
+      ) {
+         titlePlaceable.place(0, 0)
+         contentPlaceable.place(0, titlePlaceable.height)
+         buttonsPlaceable.place(0, titlePlaceable.height + contentPlaceable.height)
+      }
+   }
 }
 
 @Preview
