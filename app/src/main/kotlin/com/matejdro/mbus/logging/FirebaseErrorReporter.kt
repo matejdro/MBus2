9676f5633f2b3dbca677e2327f6623dc13b11622
Matej Drobnič <matej@matejdro.com>
2024-05-22 09:55:33 +0200

    chore: report origin of reported non-cause-exceptions

--- app/src/main/kotlin/com/matejdro/mbus/logging/FirebaseErrorReporter.kt
+++ app/src/main/kotlin/com/matejdro/mbus/logging/FirebaseErrorReporter.kt
@@ -4,24 +4,23 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.matejdro.mbus.BuildConfig
 import com.matejdro.mbus.common.di.ApplicationScope
 import com.squareup.anvil.annotations.ContributesBinding
+import si.inova.kotlinova.core.exceptions.UnknownCauseException
 import si.inova.kotlinova.core.outcome.CauseException
 import si.inova.kotlinova.core.reporting.ErrorReporter
 import javax.inject.Inject
-import kotlin.coroutines.cancellation.CancellationException
 
 @ContributesBinding(ApplicationScope::class)
 class FirebaseErrorReporter @Inject constructor() : ErrorReporter {
    private val crashlytics = FirebaseCrashlytics.getInstance()
 
    override fun report(throwable: Throwable) {
-      if (throwable is CancellationException) {
-         report(Exception("Got cancellation exception", throwable))
+      if (throwable !is CauseException) {
+         report(UnknownCauseException("Got reported non-cause exception", throwable))
          return
       }
-
       if (BuildConfig.DEBUG) {
          throwable.printStackTrace()
-      } else if (throwable !is CauseException || throwable.shouldReport) {
+      } else if (throwable.shouldReport) {
          crashlytics.recordException(throwable)
       }
    }
